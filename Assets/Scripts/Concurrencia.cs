03cd0fe13c31ace09093111377268720967fb752
alexislases06 <alexislases06@gmail.com>
2026-03-09 12:53:50 -0600

    Eventos UI y Concurrencia

---
+++ Assets/Scripts/Concurrencia.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//para los hilos
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Concurrencia : MonoBehaviour
+{
+    [Header("Activa los metodos")]
+    public bool useSincrono;
+    public bool useThread;
+    public bool useTask;
+    public bool useCoroutine;
+
+    [Header("Esfera a mover")]
+    public Transform sincronoSphere;
+    public Transform threadSphere;
+    public Transform taskSphere;
+    public Transform coroutineSphere;
+    public Transform mainCube;
+
+    //Acciones a ejecutar en el hilo secundario
+
+    private Queue<Action> mainThreadActions = new Queue<Action>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Siempre gira el cubo de referencia
+        mainCube.Rotate(Vector3.up, 50 * Time.deltaTime);
+
+        //Ejecuta las acciones en el hilo principal
+        lock (mainThreadActions)
+        {
+            while (mainThreadActions.Count > 0) { mainThreadActions.Dequeue().Invoke(); }
+        }
+    }
+
+    //Metodos para herramientas de concurrencia
+    void MoveSincrono()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            sincronoSphere.position += Vector3.right * 0.05f;
+        }
+        Thread.Sleep(50);
+    }
+
+    IEnumerator MoveWithCoroutine()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            coroutineSphere.position += Vector3.right * 0.05f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/EventUI.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+
+public class EventUI : MonoBehaviour
+{
+    public List<GameObject> listaInstrucciones;
+    public List<string> mensajesInstrucciones;
+    public TextMeshProUGUI TextMeshProUGUI;
+    public int currentIndex = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Actualizas la visibilidad de paneles
+        UpdateVisibility();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Metodos para cambiar de escena
+    public void ChangeSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+    public void ChangeSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    public void ReloadCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    //Metodo para actualizar la visibilidad de paneles
+    private void UpdateVisibility()
+    {
+        for (int i = 0; i < listaInstrucciones.Count; i++) 
+        {
+            //Solo el panel en el indice actual esta activo
+            listaInstrucciones[i].SetActive(i==currentIndex);
+        }
+    }
+
+    //Metodo para cambiar paneles
+    public void CycleObject(int direction)
+    {
+        //Incrementa o decrementa el indice y se reinicia
+        currentIndex = (currentIndex + direction + listaInstrucciones.Count) % listaInstrucciones.Count;
+
+        UpdateVisibility();
+    }
+
+    //Metodo para cambiar mensajes
+    public void CycleText(int direction)
+    {
+        //Incrementa o decrementa el indice y se reinicia
+        currentIndex = (currentIndex + direction + mensajesInstrucciones.Count) % mensajesInstrucciones.Count;
+
+        UpdateText();
+    }
+
+    //Metodo para actualizar la visibilidad de mensajes
+    private void UpdateText()
+    {
+        if(mensajesInstrucciones.Count > 0 && TextMeshProUGUI != null)
+        {
+            TextMeshProUGUI.text = mensajesInstrucciones[currentIndex];
+        }
+    }
+
+    //Metodo para salir de la aplicación
+    public void ExitGame()
+    {
+        //donde vas a hacer la impresión del mensaje
+        Debug.Log("Va a salir");
+        Application.Quit();
+        Debug.Log("Ya salió");
+    }
+
+}
